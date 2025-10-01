06b8b3f909c5f4f5474df232308d5785073c4c07
solaiman-islam-shadhin <solaimanislamshadhin123@gmail.com>
2025-10-01 13:40:02 +0600

    finish

--- src/components/Fotter.jsx
+++ src/components/Fotter.jsx
@@ -1,6 +1,10 @@
 import React from 'react'
+import { Link } from 'react-router'
+import { useAuth } from '../context/AuthContext'
 
 export const Fotter = () => {
+  const { user } = useAuth();
+
   return (
     <div>
       <footer className="footer footer-center bg-primary text-primary-content p-10">
@@ -12,6 +16,15 @@ export const Fotter = () => {
           </p>
           <p>Copyright © {new Date().getFullYear()} - All right reserved</p>
         </aside>
+        <nav>
+          <div className="grid grid-flow-col gap-4">
+            <Link to="/" className="link link-hover">Home</Link>
+            <Link to="/apps" className="link link-hover">Apps</Link>
+            {user && (
+              <Link to="/MyProfile" className="link link-hover">My Profile</Link>
+            )}
+          </div>
+        </nav>
       </footer>
     </div>
   )
